cc67aa41fa0e901f867c5e7f91a330f4f7a88a2b
Johan van Eck <johveck@gmail.com>
2015-12-24 08:35:01 +0100

    Update readme + add comments to the JS part of this extension

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # HN Sidebar
-This extension add a sidebar to pages that are on the frontpage.
-This sidebar contains the comments from HackerNews
+A friend of mine asked me about a simple sidebar for the HackerNews stories.
+He was tired of opening two tabs for each story, a comments tab and the article itself.
+That's why he asked me to make this.
+
+This Safari extension add a little toggle in the top right corner on all the frontpage article pages from HackerNews.
+When you click it you see the comments that are placed on HackerNews.
+
+## Installation
+To install this extension you need to take a few steps. These steps are:
+
+ - Clone this project
+ - Open Safari
+ - Go to preferences (CMD + ,)
+ - Click on the tap advanced
+ - Check the checkbox `Show Develop menu in menu bar`
+ - Open de develop menu and select `Show extension builder`
+ - Press on the little plus (+) in the bottom left corner and select `Add extension`
+ - Browse to the clone of the project and open de folder
+ - Click in the top right corner on `Install`
+ - Go the the HackerNews homepage and open a link to show the sidebar
+
+## Contributing
+This project is open and you can do with it what you want.
+If you want to contribute just follow the installation steps above.
+Make sure you reload the extension in Safari everytime you make a code change to see the changes.
--- js/modules/showSidebar.js
+++ js/modules/showSidebar.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+  // We need to find out if the current URL is a story on HN
+  // It's important these calls are HTTPS to make sure it works on every page
+  // Safari stops you from calling a HTTP request on a HTTPS page, and HTTPS is better so yeah....
   var itemUrl = window.location.href;
   var ajaxUrl = 'https://hn.algolia.com/api/v1/search?query=' + itemUrl + '&restrictSearchableAttributes=url&tags=front_page';
 
@@ -6,6 +9,7 @@ $(document).ready(function() {
     url: ajaxUrl
   })
   .done(function(data) {
+    // If the story is found with the current URL run another function
     if (data.hits.length > 0) {
       getComments(data.hits[0].objectID);
     }
@@ -17,12 +21,14 @@ $('html').on('click', '.hn-comments-sidebar .hn-toggle', function(event) {
 });
 
 function getComments(objectID) {
+  // The first API call didn't return comments so we need to do anthother call to get those
   var commentsUrl = 'https://hn.algolia.com/api/v1/items/' + objectID;
 
   $.ajax({
     url: commentsUrl
   })
   .done(function(data) {
+    // If there are any comments trigger the sidebar initializer
     if (data.children.length > 0) {
       initSidebar(data);
     }
@@ -30,6 +36,7 @@ function getComments(objectID) {
 }
 
 function initSidebar(data) {
+  // Append sidebar to the current page html tag
   $('html').append('<div class="hn-comments-sidebar sidebar-hidden">' +
     '<div class="hn-toggle"></div>' +
     '<div class="hn-comments">' +
@@ -41,12 +48,17 @@ function initSidebar(data) {
 function listChildren(children, depth) {
   var comments = '';
 
+  // The API returns an hash with a key children
+  // Let's check if there are children and that children is an array
+  // JS seems to be doing somethings strange here with listing all the keys when calling children
   if (children.length > 0 && Array.isArray(val.children)) {
     $.each(children, function(index, val) {
+      // API return null if no points, let's fix that to make sure we have a nice show
       if (val.points === null) {
         val.points = '0';
       }
 
+      // HTML of each comment
       comments += '<div class="hn-comment depth-' + depth + '">' +
                     '<div class="user">' +
                       val.author +
@@ -57,10 +69,12 @@ function listChildren(children, depth) {
                     '<div class="text">' +
                       val.text +
                     '</div>' +
+                    // Since we have dept in the comments we need to run this function again with a new depth
                     listChildren(val.children, depth + 1) +
                   '</div>';
     });
   }
 
+  // Return the comments HTML to the place this function is called from
   return comments;
 }
